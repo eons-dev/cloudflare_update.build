1433d344d50870780d0c81750f910266a3072e6c
eons <eons@eons.dev>
2024-08-27 13:29:28 -0700

    expand matching capabilities to include NOT

--- inc/cloudflare_update.py
+++ inc/cloudflare_update.py
@@ -245,39 +245,55 @@ class cloudflare_update(Builder):
 				setting_can_be_applied = True
 				logging.debug(f"Trying to match with {setting['match']}")
 				for key, value in setting['match'].items():
+					matched = False
+
+					invert = False
+					if (key.startswith("!")):
+						key = key[1:]
+						invert = True
+
 					if (key not in domain_config):
+						# Fatal
+						logging.error(f"Match key not found in config: {key}, config: {domain_config}")
 						setting_can_be_applied = False
 						break
+
 					configValue = domain_config[key]
+
 					if (isinstance(value, list)):
 						if (isinstance(configValue, list)): # ALL of value iff both are lists
+							matched = True
 							for v in value:
 								if (v not in configValue):
-									setting_can_be_applied = False
+									matched = False
 									break
 						elif (isinstance(configValue, str)): # ANY iff one is a string and the other is a list
-							if (configValue not in value):
-								setting_can_be_applied = False
-								break
+							matched = (configValue in value)
 						else:
+							# Fatal
 							logging.error(f"Invalid match value: {value} ({type(value)})")
 							setting_can_be_applied = False
 							break
 					elif (isinstance(value, str)):
 						if (isinstance(configValue, list)): # ANY iff one is a string and the other is a list
-							if (value not in configValue):
-								setting_can_be_applied = False
-								break
+							matched = (value in configValue)
 						elif (isinstance(configValue, str)): # == iff both are strings
-							if (value != configValue):
-								setting_can_be_applied = False
-								break
+							matched = (value == configValue)
 						else:
+							# Fatal
 							logging.error(f"Invalid match config: {configValue} ({type(configValue)})")
 							setting_can_be_applied = False
 							break
 					else:
+						# Fatal
 						logging.error(f"Invalid match value: {value} ({type(value)})")
+						setting_can_be_applied = False
+						break
+
+					if (invert):
+						matched = not matched
+
+					if (not matched):
 						setting_can_be_applied = False
 						break
 
